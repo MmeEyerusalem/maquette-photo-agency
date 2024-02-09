46d2b6351c4db1a0c329d2aed3b31df4e77b3fed
Eyerusalem Gemeda <eyerusalem.gemeda@colombbus.org>
2024-02-09 15:06:49 +0100

    maquette-photo-agency

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// function myFunction() {
+//     alert("I am an alert box!");
+//   }
+
+let iconFb = document.querySelector('.fb');
+    iconFb.addEventListener= ('mouseover', function(){
+    iconFb.style.backgroundColor="orangered";
+    iconFb.style.borderrRadius="50%";
+
+});
+// function showAlert() {
+//     alert("warninng");
+// }
+
+// showAlert()
